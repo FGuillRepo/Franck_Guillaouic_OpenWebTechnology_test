1fab83a46e682ba1a9bb9968a8c78dd325523e23
Guillaouic Franck <FOXspin@hotmail.fr>
2019-03-14 09:36:26 +0100

    Corrected Package

--- app/src/androidTest/java/com/guillaouic/test/UITest.java
+++ app/src/androidTest/java/com/guillaouic/test/UITest.java
@@ -30,8 +30,6 @@ import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.intent.Intents.intended;
-import static android.support.test.espresso.intent.matcher.IntentMatchers.hasComponent;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
